6d32f833c3d39bdaf4e21e89e2f994c316c51e59
songgyeongmin <gmsong07@gmail.com>
2025-06-03 21:52:03 +0900

    Update index.js

--- chapter02/js/index.js
+++ chapter02/js/index.js
@@ -1,8 +1,8 @@
-// C = (F - 32) / 1.8
+// 섭씨 온도 = (화씨온도 - 32) / 1.8
+// 섭씨 : celsius, 화씨 : fahrenheit
 
-let F = parseFloat(prompt("화씨 온도 입력력"));
-let C;
+let fah = parseFloat(prompt("변환할 화씨 온도"));
+let cel;
 
-C = ((F - 32) / 1.8).toFixed(1);
-
-alert(`화씨 ${F} -> 섭씨 ${C}`);
+cel = ((fah - 32) / 1.8).toFixed(1);
+alert(`화씨 ${fah}도는 섭씨 ${cel}도입니다.`);
